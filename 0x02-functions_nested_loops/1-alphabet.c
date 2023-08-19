b4504e0cd8ce043305243a98fc54a47e050e8a00
MakauLeon <makauleon21@gmail.com>
2023-08-19 23:18:37 +0300

    Added print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
