4543d14da213a9447d23d941f023d8b7230ea4f0
MakauLeon <makauleon21@gmail.com>
2023-08-20 09:30:04 +0300

    Debugging tasks

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,26 @@
+/**
+ * largest_number - returns the largest number
+ * @one: first integer
+ * @two: second integer
+ * @three: third integer
+ * Return: largest number
+ */
+int largest_number(int one, int two, int three)
+{
+	int largest;
+
+	if (one >= two && one >= three)
+	{
+		largest = one;
+	}
+	else if (two >= one && two >= three)
+	{
+		largest = two;
+	}
+	else
+	{
+		largest = three;
+	}
+
+	return (largest);
+}
